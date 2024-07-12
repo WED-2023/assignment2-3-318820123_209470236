872ffc6a58accef749c7659f047fa95ec65665d7
hallelhel <hallelh@post.bgu.ac.il>
2024-07-12 17:11:32 +0300

    sss

--- main.js
+++ main.js
@@ -31,15 +31,15 @@ app.use(
 app.use(express.urlencoded({ extended: false })); // parse application/x-www-form-urlencoded
 app.use(express.static(path.join(__dirname, "public"))); //To serve static files such as images, CSS files, and JavaScript files
 //local:
-app.use(express.static(path.join(__dirname, "dist")));
+// app.use(express.static(path.join(__dirname, "dist")));
 //remote:
-// app.use(express.static(path.join(__dirname, '../assignment-3-3-basic/dist')));
+app.use(express.static(path.join(__dirname, '../assignment2-1-318820123_209470236/dist')));
 app.get("/",function(req,res)
 { 
   //remote: 
-  // res.sendFile(path.join(__dirname, '../assignment-3-3-basic/dist/index.html'));
+  res.sendFile(path.join(__dirname, '../assignment2-1-318820123_209470236/dist/index.html'));
   //local:
-  res.sendFile(__dirname+"/index.html");
+  // res.sendFile(__dirname+"/index.html");
 
 });
 
@@ -99,14 +99,15 @@ app.use(function (err, req, res, next) {
 
 
 
-const server = app.listen(port, () => {
-  // console.log(process.env);
-  console.log(`Server listen on port ${port}`);
-});
+// const server = app.listen(port, () => {
+//   // console.log(process.env);
+//   console.log(`Server listen on port ${port}`);
+// });
 
-process.on("SIGINT", function () {
-  if (server) {
-    server.close(() => console.log("server closed"));
-  }
-  process.exit();
-});
+// process.on("SIGINT", function () {
+//   if (server) {
+//     server.close(() => console.log("server closed"));
+//   }
+//   process.exit();
+// });
+module.exports=app;
